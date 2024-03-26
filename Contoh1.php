e00031cba779860b0d4a3667e03bd79525231edc
Acepms <acepmuhamadsoleh14@gmail.com>
2024-03-27 00:49:17 +0700

    first commit

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN</h1>";
+        echo "Nama saya acep muhamad soleh
+        saya tinggal di daerah tangerang
+        hobi saya adalah mendaki";
+    }
+}
